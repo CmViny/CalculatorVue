75f63322ce6cec75ddcd7c873039daf551e705b4
VinyCm <viny.come@epitech.eu>
2022-09-06 16:10:11 +0200

    Init Projet Calculator && Structure + Style

---
+++ CalculatorProject/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app">
+
+    <div class="main">
+      <!-- CALCULATOR RESULT -->
+      <div class="calculatorResult">
+        {{current || 0}}
+      </div>
+      <!-- CALCULATOR BTN -->
+      <div class="calculatorBtn">
+        <div class="calculatorBtn_elem" v-for="elem in calculatorElem">
+          {{elem}}
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        current: "",
+        calculatorElem: ['C','*','/','-','7','8','9','+','4','5','6','%','1','2','3','=','0','.']
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
