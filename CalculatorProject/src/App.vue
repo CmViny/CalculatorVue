7f8312b13d8dd005a25c7dfbdcbb41395dc115d7
VinyCm <viny.come@epitech.eu>
2022-09-06 17:42:40 +0200

    Calculator Success

--- CalculatorProject/src/App.vue
+++ CalculatorProject/src/App.vue
@@ -8,9 +8,14 @@
       </div>
       <!-- CALCULATOR BTN -->
       <div class="calculatorBtn">
-        <div class="calculatorBtn_elem" v-for="elem in calculatorElem">
+
+        <div class="calculatorBtn_elem" v-for="elem in calculatorElem" :key="elem"
+        :class="{'green' : ['C','*','/','-','+','%','='].includes(elem)}"
+        @click="calculatorMethod(elem)"
+        >
           {{elem}}
         </div>
+
       </div>
 
     </div>
@@ -24,11 +29,43 @@
     data() {
       return {
         current: "",
-        calculatorElem: ['C','*','/','-','7','8','9','+','4','5','6','%','1','2','3','=','0','.']
+        previousCurrent: '',
+        calculatorElem: ['C','*','/','-','7','8','9','+','4','5','6','%','1','2','3','=','0','.'],
+        operator : null,
       }
     },
     methods: {
-      
+      // Ajout de la value si elem est un nombre ou un dot(.)
+      calculatorMethod(elem) {
+        if(!isNaN(elem) || elem === '.') { 
+          this.current += elem + '';
+        }
+
+        // Clear value
+        if(elem === 'C') {
+          this.current = '';
+        }
+
+        // // Modulo value
+        if(elem === '%') {
+          this.current = this.current / 100 + "";
+        }
+
+        // Operator value
+        if(['/','*','+','%','-'].includes(elem)) {
+          this.operator = elem;
+          this.previousCurrent = this.current;
+          this.current = (this.previousCurrent + this.operator)
+        }
+
+        // Result 
+        if(elem === '=') {
+          this.current = eval(this.current)
+        }
+
+        this.previousCurrent = "";
+        this.operator = null;
+      }
     }
   }
 </script>
